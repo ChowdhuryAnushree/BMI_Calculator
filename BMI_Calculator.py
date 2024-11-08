e20aaea376564a12a96860c623954165c6b83912
ChowdhuryAnushree <worklist4anushree@gmail.com>
2024-11-08 13:11:05 -0500

    Add files via upload

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -1,7 +1,6 @@
 #	Create a program for a BMI calculator that takes the name, age, height, and weight of a person and stores the data into a file.
 #	Allow the application to show weather the user is Underweight, Overweight, Obese, or Normal. Make sure you add validation rules.
 #	Add a ‘Show Graph’ button that display a pie chart of the data stored, showing the percentage of people under certain BMI categories.
-#	Challenge: Can you allow the chart to show on the same window? You may need to use another library
 
 import tkinter
 from tkinter import ttk
